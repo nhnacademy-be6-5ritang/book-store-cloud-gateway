7c476a5599f4ade0e68ae3c4913b8df814bb6b8a
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-17 14:23:41 +0900

    first

---
+++ src/main/java/com/gateway/BookStoreCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreCloudGatewayApplication.class, args);
+	}
+
+}
