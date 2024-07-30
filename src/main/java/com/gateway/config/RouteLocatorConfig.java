a6a78df1e0bd2088520877c2b9ed896b08c7e8bc
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-30 21:36:37 +0900

    feat: 불필요 주입 제거

--- src/main/java/com/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/gateway/config/RouteLocatorConfig.java
@@ -1,17 +1,12 @@
 package com.gateway.config;
 
-import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
 public class RouteLocatorConfig {
-	private final SpringCloudCircuitBreakerFilterFactory circuitBreakerFilterFactory;
 
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
