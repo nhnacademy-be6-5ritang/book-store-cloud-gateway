5f2c5959d02a4a8d78802736c5b61e977ba96379
kimdaun <fhdlwp15@naver.com>
2024-08-01 12:35:19 +0900

    test : 테스트 추가

--- src/test/java/com/gateway/BookStoreCloudGatewayApplicationTests.java
+++ src/test/java/com/gateway/BookStoreCloudGatewayApplicationTests.java
@@ -1,5 +1,7 @@
 package com.gateway;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BookStoreCloudGatewayApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void mainMethodTest() {
+		assertDoesNotThrow(() -> {
+			BookStoreCloudGatewayApplication.main(new String[]{});
+		});
 	}
 
 }
