74f55d72cffdd31c2ef3637dc104e84213e95c44
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-08-30 04:47:21 +0900

    feat: 라우터 이중화 환경 분리

--- src/main/java/com/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/gateway/config/RouteLocatorConfig.java
@@ -4,23 +4,40 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class RouteLocatorConfig {
 
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-			.route("bookstore-coupon", r -> r.path("/coupons/**")
-				.uri("http://localhost:9494")
-			)
-			.route("bookstore-back", r -> r.path("/api/**")
-				.uri("lb://book-store-back")
-			)
-			.route("bookstore-account", r -> r.path("/auth/**")
-				.uri("http://localhost:8070")
-			)
-			.build();
-	}
+    @Bean
+    @Profile("prod")
+    public RouteLocator prodRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("bookstore-coupon", r -> r.path("/coupons/**")
+                        .uri("http://localhost:9494")
+                )
+                .route("bookstore-back", r -> r.path("/api/**")
+                        .uri("lb://book-store-back") // 로드밸런싱 활성화
+                )
+                .route("bookstore-account", r -> r.path("/auth/**")
+                        .uri("http://localhost:8070")
+                )
+                .build();
+    }
 
+    @Bean
+    @Profile("dev")
+    public RouteLocator devRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("bookstore-coupon", r -> r.path("/coupons/**")
+                        .uri("http://localhost:9494")
+                )
+                .route("bookstore-back", r -> r.path("/api/**")
+                        .uri("http://localhost:8083") // 로드밸런싱 비활성화, dev 환경의 로컬 서버
+                )
+                .route("bookstore-account", r -> r.path("/auth/**")
+                        .uri("http://localhost:8070")
+                )
+                .build();
+    }
 }
